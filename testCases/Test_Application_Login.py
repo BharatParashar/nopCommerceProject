590df0115b77a12b26d00c39cde5ddcffa814aba
Bharat <BharatB.Parashar@gmail.com>
2023-05-31 21:21:18 +0530

    Added Login TestCase

--- testCases/Test_Application_Login.py
+++ testCases/Test_Application_Login.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from pageObjects.LoginPage import LoginPage
 from testCases.BaseTest import BaseTest
+from utilities.configReader import getvaluefromconfig
 
 
 class TestLogin(BaseTest):
@@ -10,3 +11,10 @@ class TestLogin(BaseTest):
     def test_verify_application_title(self):
         self.objlogin = LoginPage(self.driver)
         assert self.objlogin.getpagetitle() == "Your store. Login"
+
+
+    def test_login_application(self):
+        self.objlogin = LoginPage(self.driver)
+        self.objlogin.enter_user_name(getvaluefromconfig("Parameters","UserName"))
+        self.objlogin.enter_password(getvaluefromconfig("Parameters", "Password"))
+        self.objlogin.click_on_login()
